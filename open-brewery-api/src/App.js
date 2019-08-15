8b835b3a523d4ba382f8babed770ef5ce6a20db5
ch04937 <97hernandez.c@gmail.com>
2019-08-15 14:48:33 -0700

    initial commit project set up

---
+++ open-brewery-api/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import OpenBrewery from './components/OpenBrewery'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <OpenBrewery />
+    </div>
+  );
+}
+
+export default App;
